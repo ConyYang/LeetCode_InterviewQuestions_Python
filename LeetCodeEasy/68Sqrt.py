6b5ea70aa715638aaa2cc72d041c8d7a39793dac
Yubei Yang <58965389+ConyYang@users.noreply.github.com>
2020-08-04 17:49:19 +0800

    Add 68 Sqrt(x)

---
+++ LeetCodeEasy/68Sqrt.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 2:
+            return x
+        left, right = 1, x // 2
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid > x / mid:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left - 1
+
+a = Solution()
+result = a.mySqrt(x=23901230)
+print(result)
