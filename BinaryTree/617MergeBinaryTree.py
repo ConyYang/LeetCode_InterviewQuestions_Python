172474d3cc6f29d431ab4608bf068a8c08f16be2
ConyYang <yangyubei0218@gmail.com>
2021-06-20 11:52:43 +0800

    tree

---
+++ BinaryTree/617MergeBinaryTree.py
@@ -0,0 +1,22 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def mergeTrees(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: TreeNode
+        """
+        if root1 == None: return root2
+        if root2 == None: return root1
+
+        root = TreeNode(root1.val + root2.val)
+        root.left = self.mergeTrees(root1.left, root2.left)
+        root.right = self.mergeTrees(root1.right, root2.right)
+
+        return root
